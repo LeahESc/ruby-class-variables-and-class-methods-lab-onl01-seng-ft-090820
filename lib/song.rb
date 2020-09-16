06ca1bb920507c6b7fded6001d749cd230ab9f6d
Leah Schlackman <leahemily1@gmail.com>
2020-09-16 19:04:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   
   def self.genre_count
     genre_h = {}
-     @@genres.collect {|genre|}
-     genre_h[genre] 
+     @@genres.collect {|item|
+     genre_h[genre] => " "}
   end
 end 
